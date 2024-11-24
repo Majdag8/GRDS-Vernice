b39724d6b569759104be1d94345e596c7895e9e8
Majdag8 <majdag2005@gmail.com>
2024-11-24 03:46:51 +0200

    booking popup done

--- Home-Page.js
+++ Home-Page.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+    $('#back-form img').on('click', function() {
+        $('#booking-form').hide();
+    });
+
+    $('#booking-table-button').on('click', function(){
+        $('#booking-form').show();
+    })
+    
     $('#create-account').on('click', function(event) {
         event.preventDefault();
         $('#loyalty-form').hide();
