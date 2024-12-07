c7589f3dcca902e17e901495881f841a8209a124
Majdag8 <majdag2005@gmail.com>
2024-12-07 19:28:58 +0200

    updates

--- Home-Page.js
+++ Home-Page.js
@@ -14,4 +14,36 @@ $(document).ready(function() {
     $("#homedocopage").click(() => {
         window.location.href = 'Home-Deco.html';
     })
+
+    $("#landing button").click(function () {
+        window.location.href = "Workshop.html";
+    })
+
+    $("#booking-form #decrement").on('click', function () {
+        let counter = $('#bookingcounterValue');
+        let currentValue = parseInt(counter.text());
+        if (currentValue > 1) {
+            counter.text(currentValue - 1);
+        }
+    })
+
+    $("#booking-form #increment").on('click', function () {
+        let counter = $('#bookingcounterValue');
+        let currentValue = parseInt(counter.text());
+        counter.text(currentValue + 1);
+    })
+
+    $("#counter #decrement").on('click', function (event) {
+        let counter = event.target.nextElementSibling;
+        let currentValue = parseInt(counter.textContent);
+        if (currentValue > 1) {
+            counter.textContent = currentValue - 1;
+        }
+    })
+
+    $("#counter #increment").on('click', function (event) {
+        let counter = event.target.previousElementSibling;
+        let currentValue = parseInt(counter.textContent);
+        counter.textContent = currentValue + 1;
+    })
 });
--- header.js
+++ header.js
@@ -25,38 +25,6 @@ $(document).ready(function () {
         }
     });
 
-    $("#landing button").click(function () {
-        window.location.href = "Workshop.html";
-    })
-
-    $("#booking-form #decrement").on('click', function () {
-        let counter = $('#bookingcounterValue');
-        let currentValue = parseInt(counter.text());
-        if (currentValue > 1) {
-            counter.text(currentValue - 1);
-        }
-    })
-
-    $("#booking-form #increment").on('click', function () {
-        let counter = $('#bookingcounterValue');
-        let currentValue = parseInt(counter.text());
-        counter.text(currentValue + 1);
-    })
-
-    $("#counter #decrement").on('click', function (event) {
-        let counter = event.target.nextElementSibling;
-        let currentValue = parseInt(counter.textContent);
-        if (currentValue > 1) {
-            counter.textContent = currentValue - 1;
-        }
-    })
-
-    $("#counter #increment").on('click', function (event) {
-        let counter = event.target.previousElementSibling;
-        let currentValue = parseInt(counter.textContent);
-        counter.textContent = currentValue + 1;
-    })
-
     $(document).on('click', '#login button', function () {
 
         $('#profile').css({
